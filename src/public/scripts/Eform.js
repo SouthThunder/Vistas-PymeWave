9af9b5f2fcab668ea5de393e285acbe41a6a24f9
Daniel <castedaniel29@gmail.com>
2023-04-14 01:53:49 -0500

    manejo formulario y agregado categorias

--- src/public/scripts/Eform.js
+++ src/public/scripts/Eform.js
@@ -20,26 +20,37 @@ var regexEmail = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/;
     if(!correo.match(regexEmail)){
         setErrorFor(document.getElementById('Correo'),'Por favor, Ingrese una dirección de correo válida');
         sePudo=false;
+        
+    }else{
+        setSuccesFor(document.getElementById('Correo'));
     }
     //Valida que el telefono tenga únicamente números
     if(!telefono.match(regexNumeros)){
         setErrorFor(document.getElementById('Telefono'),'El telefono debe contener únicamente números.');
         sePudo=false;
+    }else{
+        setSuccesFor(document.getElementById('Telefono'));
     }
     //Valida que el telefono sea de 10 numeros(En colombia los numeros son de 10 números)
     if(telefono.length!=10){
         setErrorFor(document.getElementById('Telefono'),'Por favor, Ingrese un télefono válido.');
         sePudo=false;
+    }else{
+        setSuccesFor(document.getElementById('Telefono'));
     }
     //Valida que la contraseña tenga más de 8 caracteres
     if(password.length<8){
         setErrorFor(document.getElementById('password'),'La contraseña debe contener mínimo 8 caracteres');
         sePudo=false;
+    }else{
+        setSuccesFor(document.getElementById('password'));
     }
     //Valida condiciones de contraseña
     if(!password.match(regexMayuscula)||!password.match(regexMinuscula)||!password.match(regexNumero)||!password.match(regexCaracterEspecial) ){
         setErrorFor(document.getElementById('password'),'Contraseña requiere mayúscula, minúscula, número y caracter especial.');
         sePudo=false;
+    }else{
+        setSuccesFor(document.getElementById('password'));
     }
 
 if(sePudo==false){
@@ -55,6 +66,10 @@ function setErrorFor(input,message){
     small.innerText = message;
 
     input.focus();
+}
+function setSuccesFor(input){
+    const formControl= input.parentElement;
+    formControl.className= 'inputbox sucess';
 }
 form.addEventListener("submit",e => {
     e.preventDefault(); 
--- src/public/scripts/Pform.js
+++ src/public/scripts/Pform.js
@@ -20,38 +20,54 @@ var regexEmail = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/;
 //Valida que sea un email
     if(!correo.match(regexEmail)){
         setErrorFor(getElementById('Correo').value,'Por favor, Ingrese una dirección de correo válida');
+        console.log("ss");
         sePudo=false;
+    }
+    else{
+        setSuccesFor(getElementById('Correo').value);
     }
     //valida que el nombre tenga unicamente letras
     if(!nombre.match(regexSoloLetras)||!apellidos.match(regexSoloLetras)){
         setErrorFor(getElementById('nombre').value,'Por favor, digite un nombre y/ o apellido valido');
         sePudo=false;
     }
+    else{
+        setSuccesFor(getElementById('nombre').value);
+    }
     //Valida que el telefono tenga únicamente números
     if(!telefono.match(regexNumeros)){
         setErrorFor(getElementById('Telefono').value,'El telefono debe contener únicamente números.');
         sePudo=false;
     }
+    else{
+        setSuccesFor(getElementById('Telefono').value);
+    }
     //Valida que el telefono sea de 10 numeros(En colombia los numeros son de 10 números)
     if(telefono.length!=10){
         setErrorFor(getElementById('Telefono').value,'Por favor, Ingrese un télefono válido.');
         sePudo=false;
+    }else{
+        setSuccesFor(getElementById('Telefono').value);
     }
     //Valida que la contraseña tenga más de 8 caracteres
     if(password.length<8){
         setErrorFor(getElementById('password').value,'La contraseña debe contener mínimo 8 caracteres');
         sePudo=false; 
+    }else{
+        setSuccesFor(getElementById('password').value);
     }
     //Valida condiciones de contraseña
     if(!password.match(regexMayuscula)||!password.match(regexMinuscula)||!password.match(regexNumero)||!password.match(regexCaracterEspecial) ){
         setErrorFor(getElementById('password').value,'Contraseña requiere mayúscula, minúscula, número y caracter especial.');
         sePudo=false;
+    }else{
+        setSuccesFor(getElementById('password').value);
     }
 
-    if(sePudo==true){
-        return true;
-    }else{
+    if(sePudo==false){
         return false;
+    }else{
+        return true;
     }
     
 }
@@ -63,6 +79,10 @@ function setErrorFor(input,message){
     small.innerText = message;
 
     input.focus();
+}
+function setSuccesFor(input){
+    const formControl= input.parentElement;
+    formControl.className= 'inputbox sucess';
 }
 
 form.addEventListener("submit",e => {
